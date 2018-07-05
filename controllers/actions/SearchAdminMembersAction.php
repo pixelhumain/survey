257603ae670d16691e8f3bfccbf89846fe1b2064
codev <raphaelriviere06@gmail.com>
2018-07-05 17:14:38 +0300

    Modif

---
+++ controllers/actions/SearchAdminMembersAction.php
@@ -0,0 +1,29 @@
+<?php
+class SearchAdminMembersAction extends CTKAction{
+    public function run(){
+    	if ( Person::logguedAndValid() ) {
+    		//$form = PHDB::findOne( Form::COLLECTION , array("id"=>$_POST["parentSurvey"]));
+    		$form = Form::getById($_POST["parentSurvey"],$fields=array("links", "author"));
+            if(	Form::canAdmin($_POST["parentSurvey"], $form)  ){
+
+            	$queryId = array("links.forms.".(String)$form["_id"]=> array('$exists' => 1) );
+            	if (!empty($_POST["text"])){
+            		$text = $_POST['text'];
+            		$queryText = array( "name" => new MongoRegex("/.*{$text}.*/i"));
+            		$query = array('$and' => array( $queryId , $queryText ) );
+            	}else
+            		$query = $queryId;
+            	
+                $persons = Person::getWhere($query);
+                $orgas = Organization::getWhere($query);
+                $results = array_merge($persons, $orgas);
+	    		Rest::json($results); exit ;
+
+			} else 
+				$this->getController()->render("co2.views.default.unauthorised"); 
+		} else
+			$this->getController()->render("co2.views.default.loginSecure");
+    }
+
+
+}
