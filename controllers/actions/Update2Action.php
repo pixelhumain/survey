a439272b4dd84785ca10f7b125d2b2d140dc860e
codev <raphaelriviere06@gmail.com>
2018-08-29 11:39:35 +0400

    Hotfix delete project d'une fiche action

--- controllers/actions/Update2Action.php
+++ controllers/actions/Update2Action.php
@@ -11,25 +11,30 @@ class Update2Action extends CAction
             //canEDitAction
             if(!empty($el)){
                 $key = $_POST["answerSection"];
-                $value = $_POST["answers"];
+                $value = (!empty($_POST["answers"]) ? $_POST["answers"] : null);
                 $verb = '$set';
+
                 if( @$_POST["arrayForm"] ){
                     $verb = '$addToSet';
                     if($value == null)
                         $verb = '$unset';
                 }
+
                 if( $value != null && !@$_POST["pull"] ) {
                     $value["created"] = time();
                     $value["user"] = Yii::app()->session["userId"];
                 }
+
                 PHDB::update($_POST["collection"],
                     array("_id"=>new MongoId((string)$el["_id"])), 
                     array($verb => array($key => $value)));
                 
-                if($value == null && @$_POST["pull"] )
-                    PHDB::update($_POST["collection"],
-                    array("_id"=>new MongoId((string)$el["_id"])), 
-                    array('$pull' => array($_POST["pull"] => null )));
+                if($value == null && @$_POST["pull"] ){
+                    PHDB::update(   $_POST["collection"],
+                                    array("_id"=>new MongoId((string)$el["_id"])), 
+                                    array('$pull' => array($_POST["pull"] => null )));
+                }
+                    
 
                 $msg=Yii::t("common","Evrything allRight");
                 $res=true;
