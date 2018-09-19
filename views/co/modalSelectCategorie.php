cc4a88f6fb14c521d8023a01f45c54e94a65072a
codev <raphaelriviere06@gmail.com>
2018-09-19 20:53:09 +0400

    Hotfix

--- views/co/modalSelectCategorie.php
+++ views/co/modalSelectCategorie.php
@@ -6,9 +6,14 @@
             </div>
             <div class="modal-body center text-dark hidden" id="modalRegisterSuccessContent"></div>
             <div class="modal-body center text-dark">
-                
                 <h5><i class="fa fa-angle-down"></i> Catégorie</h5>
                 <input id="selectCategorie" class="" type="text" data-type="select2" name="roles" placeholder="Choisissez une catégorie" style="width:100%;">
+                 <input type="hidden" name="answerId" id="answerId" value=""/>
+                 <input type="hidden" name="eligible" id="eligible" value=""/>
+            </div>
+            <div class="modal-body center text-dark">
+                <h5><i class="fa fa-angle-down"></i> Tags</h5>
+                <input id="selectTag" class="" type="text" data-type="select2" name="tags" placeholder="Choisissez une catégorie" style="width:100%;">
                  <input type="hidden" name="answerId" id="answerId" value=""/>
                  <input type="hidden" name="eligible" id="eligible" value=""/>
             </div>
@@ -29,7 +34,17 @@
         if(typeof form != "undefined" && typeof form.custom != "undefined" && typeof form.custom.roles != "undefined" && notNull(form.custom.roles) && form.custom.roles.length > 0)
             rolesList = form.custom.roles ;
 
+        var tagsC = {};
+        if(typeof form != "undefined" && typeof form.custom != "undefined" && typeof form.custom.tags != "undefined" && notNull(form.custom.tags) && form.custom.tags.length > 0)
+            tagsC = form.custom.tags ;
+
         $('#modalCatgeorieAnswers #selectCategorie').select2({tags:rolesList});
+        $('#modalCatgeorieAnswers #selectTag').select2({
+            tags:tagsC,
+            maximumSelectionLength : 1
+        });
+
+
         $("#modalCatgeorieAnswers #validEligible").on("click",function(e){
             var params = {
                 answerId : $("#answerId").val(),
@@ -37,7 +52,8 @@
                 formId : form.id,
                 session : formSession,
                 eligible : $("#eligible").val(),
-                roles : $("#selectCategorie").val()
+                roles : $("#selectCategorie").val(),
+                tags : $("#selectTag").val()
             };
 
             eligibleFct(params);
